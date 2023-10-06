2e579c861e641950673db197393078bf04c02787
Hassan220022 <hassansonson2002@gmail.com>
2023-10-06 21:42:19 +0300

    add 9print

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,8 +5,9 @@
  *
  * Description: This function prints the lowercase alphabet.
  *
+ * Return: Always 0 (Success)
  */
-void main(void)
+int main(void)
 {
 char m;
 for (m = 0; m <= 9; m++)
@@ -14,7 +15,9 @@ for (m = 0; m <= 9; m++)
 putchar(m + '0');
 putchar(',');
 putchar(' ');
+
 }
 putchar('\n');
 
+return (0);
 }
