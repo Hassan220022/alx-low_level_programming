14480b4e868bb43a55073016f29443a35ec3bc65
Hassan220022 <hassansonson2002@gmail.com>
2023-10-11 02:47:37 +0300

    modified:   0x02-functions_nested_loops/5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,7 @@
 
 /**
  * print_sign - Prints the sign of a number.
- * @n: The number to be checked.
+ * @value: The number to be checked.
  *
  * Return: 1 if n is greater than zero, 0 if n is zero, -1 if n is less than
  * zero.
