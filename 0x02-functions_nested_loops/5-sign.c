0c97b156d8cbf699e11836e141739ae7b3dcad32
Hassan220022 <hassansonson2002@gmail.com>
2023-10-11 02:46:08 +0300

    modified:   0x02-functions_nested_loops/5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,14 +11,17 @@ int print_sign(int value)
 {
 if (value == 0)
 {
+_putchar('0');
 return (0);
 }
 else if (value > 0)
 {
+_putchar('+');
 return (1);
 }
 else
 {
+_putchar('-');
 return (-1);
 }
 }
