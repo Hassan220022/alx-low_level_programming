383c3149f5154f23fb59c24f1ed845947240da78
Hassan220022 <hassansonson2002@gmail.com>
2023-10-10 22:08:19 +0300

    new file:   0x02-functions_nested_loops/1-alphabet.c new file:   0x02-functions_nested_loops/1-main.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main()
+{
+char c;
+for (char i = 'a'; i <= 'z'; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
