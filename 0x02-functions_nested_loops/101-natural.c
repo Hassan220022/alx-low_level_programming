c7b80102eea94a066cb055d76000f2076e90ff76
Hassan220022 <hassansonson2002@gmail.com>
2023-10-11 12:56:06 +0300

    modified:   0x02-functions_nested_loops/101-natural.c deleted:    0x02-functions_nested_loops/102-main.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-
+#include "main.h"
 /**
  * main - Prints the sum of all multiples of 3 or 5 below 1024.
  *
  * Return: Always 0.
  */
-void _start(void)
+int main(void)
 {
 int limit = 1024;
 int sum = 0;
@@ -19,4 +19,5 @@ sum += i;
 }
 }
 printf("The sum of all multiples of 3 or 5 below 1024 is: %d\n", sum);
+return (0);
 }
