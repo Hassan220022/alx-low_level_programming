db7a1751b104cf72b39bf5a15bfd4a462aec9e37
Hassan220022 <hassansonson2002@gmail.com>
2023-10-11 04:17:07 +0300

    ions_nested_loops/9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,32 +4,32 @@
  */
 void times_table(void)
 {
-   int i, j, value;
+int i, j, value;
 
-   for (i = 0; i <= 9; i++)
-   {
-      for (j = 0; j <= 9; j++)
-      {
-         value = (i * j);
-         if (j == 0)
-         {
-            _putchar(value + '0');
-         }
-         else if (value < 10)
-         {
-            _putchar(',');
-            _putchar(' ');
-            _putchar(' ');
-            _putchar(value + '0');
-         }
-         else
-         {
-            _putchar(',');
-            _putchar(' ');
-            _putchar((value / 10) + '0');
-            _putchar((value % 10) + '0');
-         }
-      }
-      _putchar('\n');
-   }
+for (i = 0; i <= 9; i++)
+{
+for (j = 0; j <= 9; j++)
+{
+value = (i * j);
+if (j == 0)
+{
+_putchar(value + '0');
+}
+else if (value < 10)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(value + '0');
+}
+else
+{
+_putchar(',');
+_putchar(' ');
+_putchar((value / 10) + '0');
+_putchar((value % 10) + '0');
+}
+}
+_putchar('\n');
+}
 }
