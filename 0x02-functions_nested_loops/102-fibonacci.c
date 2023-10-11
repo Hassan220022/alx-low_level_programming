b321d37c7c97ee74e792154c771e8d19c8b8a97d
Hassan220022 <hassansonson2002@gmail.com>
2023-10-11 13:12:40 +0300

    modified:   0x02-functions_nested_loops/102-fibonacci.c new file:   0x02-functions_nested_loops/103-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include "main.h"
 
 /**
  * main - Prints the sum of all even Fibonacci numbers up to 4,000,000.
@@ -8,18 +7,18 @@
  */
 int main(void)
 {
-int long first = 0, second = 1, sum = 0;
-int i;
+   int long first = 0, second = 1, sum = 0;
+   int i;
 
-for (i = 0; i < 50; i++)
-{
-sum = first + second;
-first = second;
-second = sum;
-if (i != 49)
-printf("%ld, ", sum);
-else
-printf("%ld\n", sum);
-}
-return (0);
+   for (i = 0; i < 50; i++)
+   {
+      sum = first + second;
+      first = second;
+      second = sum;
+      if (i != 49)
+         printf("%ld, ", sum);
+      else
+         printf("%ld\n", sum);
+   }
+   return (0);
 }
