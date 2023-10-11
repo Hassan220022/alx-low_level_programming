f7e87da046a4f6a250ccdcf5dec825cf75a4e24f
Hassan220022 <hassansonson2002@gmail.com>
2023-10-11 04:50:56 +0300

    0x02-functions_nested_loops/100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,28 +7,25 @@
  */
 void print_times_table(int n)
 {
-int i, j;
-
 if (n >= 0 && n <= 15)
 {
+int i, j, result;
+
 for (i = 0; i <= n; i++)
 {
 for (j = 0; j <= n; j++)
 {
-int result = i * j;
-printf("%d", result);
-
-if (j < n)
+result = i * j;
+if (j == 0)
 {
-printf(", ");
-if (result < 10)
-printf(" ");
+printf("%d", result);
 }
 else
 {
+printf(", %3d", result);
+}
+}
 printf("\n");
-}
-}
 }
 }
 }
