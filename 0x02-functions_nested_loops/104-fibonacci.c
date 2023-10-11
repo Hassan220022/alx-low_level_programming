19c0e79ef0640c9c1a4d81a250f27754336fbe3f
Hassan220022 <hassansonson2002@gmail.com>
2023-10-11 14:36:06 +0300

    modified:   0x02-functions_nested_loops/104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,19 +7,19 @@
  */
 int main(void)
 {
-   unsigned long int a = 1, b = 2, next;
-   int i;
+    unsigned long int a = 1, b = 2, next;
+    int i;
 
-   printf("%lu, %lu", a, b);
+    printf("%lu, %lu", a, b);
 
-   for (i = 3; i <= 100; i++)
-   {
-      next = a + b;
-      printf(", %lu", next);
-      a = b;
-      b = next;
-   }
+    for (i = 3; i <= 98; i++)
+    {
+        next = a + b;
+        printf(", %lu", next);
+        a = b;
+        b = next;
+    }
 
-   printf("\n");
-   return (0);
+    printf("\n");
+    return (0);
 }
