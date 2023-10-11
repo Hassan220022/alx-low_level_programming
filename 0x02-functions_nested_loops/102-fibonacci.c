70a88ba59219363929411276b3845198edc1033c
Hassan220022 <hassansonson2002@gmail.com>
2023-10-11 13:13:16 +0300

    modified:   0x02-functions_nested_loops/102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,18 +7,18 @@
  */
 int main(void)
 {
-   int long first = 0, second = 1, sum = 0;
-   int i;
+int long first = 0, second = 1, sum = 0;
+int i;
 
-   for (i = 0; i < 50; i++)
-   {
-      sum = first + second;
-      first = second;
-      second = sum;
-      if (i != 49)
-         printf("%ld, ", sum);
-      else
-         printf("%ld\n", sum);
-   }
-   return (0);
+for (i = 0; i < 50; i++)
+{
+sum = first + second;
+first = second;
+second = sum;
+if (i != 49)
+printf("%ld, ", sum);
+else
+printf("%ld\n", sum);
+}
+return (0);
 }
