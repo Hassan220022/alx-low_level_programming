3a00ed8527d187015c4ac39af1aaefb6dfeaaceb
Hassan220022 <hassansonson2002@gmail.com>
2023-10-13 05:24:10 +0300

    0x04-more_functions_nested_loops/6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,24 +1,14 @@
 #include "main.h"
-
 /**
- * print_line - Print a line of '_' characters.
- * @n: The number of times the character '_' should be printed.
- */
+*print_line - prints a straight line
+*@n:parameter
+*Return:returns nothing
+*/
 void print_line(int n)
 {
-     int i;
-
-     if (n <= 0) 
-     {
-          /* If n is 0 or less, print only a newline character */
-          _putchar('\n');
-     } 
-     else 
-     {
-          for (i = 0; i < n; i++) 
-          {
-               _putchar('_');
-          }
-          _putchar('\n');
-     }
+while (n-- > 0)
+{
+_putchar('_');
+}
+_putchar('\n');
 }
