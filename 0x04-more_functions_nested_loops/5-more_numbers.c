cd0066045d115542dee7ac529eeaa0118c91e49c
Hassan220022 <hassansonson2002@gmail.com>
2023-10-13 05:05:57 +0300

    0x04-more_functions_nested_loops/5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,9 +12,8 @@ void more_numbers(void)
 
      for (i = '0'; i <= '9'; i++)
      {
-         for (j = '0'; j <= '9'; j++)
+         for (j = '0'; j <= '14'; j++)
          {
-             _putchar(i);
              _putchar(j);
          }
          _putchar('\n');
