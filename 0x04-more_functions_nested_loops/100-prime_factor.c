d2a4ab60fb913ab39cd378510ecefbeabc0d2386
Hassan220022 <hassansonson2002@gmail.com>
2023-10-13 06:02:51 +0300

    04-more_functions_nested_loops/100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,22 +1,30 @@
-#include "main.h"
-/**
- * print_number - prints an integer
- * @n:.input integer parameter
- *
- *Owned by Bwave ICT/ Bright Daniel
- */
-void print_number(int n)
-{
-	unsigned int i = n;
+#include <stdio.h>
 
-	if (n < 0)
-	{
-		_putchar(45);
-		i = -i;
-	}
-	if (i / 10)
-	{
-		print_number(i / 10);
-	}
-	_putchar(i % 10 + '0');
+/**
+ * main - entry point
+ * Description: Prints the largest prime factor of the number 612852475143
+ * Return: 0
+ */
+int main(void)
+{
+     long int n;
+     int i;
+
+     n = 612852475143;
+     while (n % 2 == 0)
+     {
+          n /= 2;
+     }
+
+     for (i = 3; i <= n; i += 2)
+     {
+          while (n % i == 0 && n != i)
+          {
+               n /= i;
+          }
+     }
+
+     printf("%ld\n", n);
+
+     return 0;
 }
