4aaa713d6e3aef07d095f963026e6ab93eb4d9cc
Hassan220022 <hassansonson2002@gmail.com>
2023-10-13 04:55:58 +0300

    0x04-more_functions_nested_loops/4-main.c 0x04-more_functions_nested_loops/4-print_most_numbers.c 0x04-more_functions_nested_loops/main.h

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: string to be appended to
+ * @src: string to append
+ * Return: pointer to dest
+ */
+void print_most_numbers(void)
+{
+     char c;
+
+     for (c = '0'; c <= '9'; c++)
+     {
+          if (c != '2' && c != '4')
+          {
+               _putchar(c);
+          }
+     }
+}
