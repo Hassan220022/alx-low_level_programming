7fcc562075c6f507ea437af98b312bcb83cd4143
Hassan220022 <hassansonson2002@gmail.com>
2023-10-24 01:21:31 +0300

    0x07-pointers_arrays_strings/main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -12,6 +12,6 @@ void remove_duplicates(char *str);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
-char _putchar(char c);
+int _putchar(char c);
 
 #endif /* MAIN_H */
