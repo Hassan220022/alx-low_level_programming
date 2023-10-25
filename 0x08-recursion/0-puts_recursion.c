28431516287a3447856aa6893a904227cff58672
Hassan220022 <hassansonson2002@gmail.com>
2023-10-25 14:54:52 +0300

    0-put_recursino

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,11 +6,11 @@
  */
 void _puts_recursion(char *s)
 {
-	int i;
-
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		_putchar(s[i]);
-	}
-	_putchar('\n');
+	if (*s == '\0')
+     {
+          _putchar('\n');
+          return;
+     }
+     _puts_recursion(s + 1);
+     _putchar(*s);
 }
