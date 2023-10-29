ebb7a837a4bf6da010b29c8aa57541aff1d97514
Hassan220022 <hassansonson2002@gmail.com>
2023-10-30 00:32:07 +0200

    bor3i

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,6 +1,5 @@
-
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef FILE_MAIN
+#define FILE_MAIN
 
 #include <stdio.h>
 
@@ -25,4 +24,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif /* MAIN_H */
+#endif
