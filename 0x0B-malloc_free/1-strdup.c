caaa1512361739b98332c1db089923630235079a
Hassan220022 <hassansonson2002@gmail.com>
2023-10-30 20:59:18 +0200

    bor3i

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * _strdup - returns a pointer to a newly allocated space in memory,
- * which contains a copy of the string given as a parameter
- * @str: string to copy
- *
- * Return: pointer to new string, or NULL if str = NULL or insufficient memory
- */
+* _strdup - returns a pointer to a newly allocated space in memory,
+* which contains a copy of the string given as a parameter
+* @str: string to copy
+*
+* Return: pointer to new string, or NULL if str = NULL or insufficient memory
+*/
 char *_strdup(char *str)
 {
 	char *arr = NULL;
@@ -14,6 +14,11 @@ char *_strdup(char *str)
 	size_t len_str = strlen(str);
 
 	arr = (char *)malloc(sizeof(char) * (len_str + 1));
+	if (arr == NULL)
+	{
+		printf("failed to allocate memory\n");
+		return (NULL);
+	}
 
 	strcpy(arr, str);
 
