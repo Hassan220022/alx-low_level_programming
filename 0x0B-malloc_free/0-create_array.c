f0b1c3a8381b26db0f2d2787f96b54fbcd7f33b7
Hassan220022 <hassansonson2002@gmail.com>
2023-10-30 20:45:52 +0200

    :   0x0B-malloc_free/0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -19,7 +19,7 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 	}
 
-	*arr = (char *)malloc(sizeof(char) * size);
+	arr = (char *)malloc(sizeof(char) * size);
 
 	if (arr == NULL)
 	{
