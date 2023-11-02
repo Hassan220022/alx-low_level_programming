fabe6551cda6eb7976ce8cf19589000a4a440ef9
Hassan220022 <hassansonson2002@gmail.com>
2023-11-02 20:54:39 +0200

    bor3i

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -30,6 +30,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return (NULL);
 	strcpy(ptr, s1);
 	strcpy(ptr + len1, s2);
-	ptr[len1 + n - 1] = '\0';
+	ptr[len1 + n] = '\0';
 	return (ptr);
 }
