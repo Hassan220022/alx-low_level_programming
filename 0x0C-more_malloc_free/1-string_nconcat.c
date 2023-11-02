1c48b2e05c7bc4c6fac78fe43af80420f599f851
Hassan220022 <hassansonson2002@gmail.com>
2023-11-02 21:11:53 +0200

    bor3i

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * string_nconcat - concatenates two strings
- * @s1: first string
- * @s2: second string
- * @n: number of bytes of s2 to concatenate to s1
- *
- * Return: pointer to new string, NULL if it fails
- */
+* string_nconcat - concatenates two strings
+* @s1: first string
+* @s2: second string
+* @n: number of bytes of s2 to concatenate to s1
+*
+* Return: pointer to new string, NULL if it fails
+*/
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int len1, len2;
+	unsigned int len1 = 0, len2 = 0 ,i;
 
 	char *ptr;
 
@@ -18,9 +18,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-
-	len1 = strlen(s1);
-	len2 = strlen(s2);
+	while (s1[len1] != '\0')
+		len1++;
+	while (s2[len2] != '\0')
+		len2++;
 
 	if (n >= len2)
 		n = len2;
@@ -28,8 +29,17 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	ptr = malloc(sizeof(char) * len1 + n + 1);
 	if (ptr == NULL)
 		return (NULL);
-	strcpy(ptr, s1);
-	strcpy(ptr + len1, s2);
+	while (s1[i] != '\0')
+	{
+		ptr[i] = s1[i];
+		i++;
+	}
+	while (i < len1 + n)
+	{
+		ptr[i] = s2[i];
+		i++;
+	}
+
 	ptr[len1 + n] = '\0';
 	return (ptr);
 }
