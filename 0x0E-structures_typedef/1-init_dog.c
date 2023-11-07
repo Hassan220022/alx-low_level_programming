7b8bfbf82d1bf105af67c53422597094f500578d
Hassan220022 <hassansonson2002@gmail.com>
2023-11-07 09:39:48 +0200

    .vscode/settings.json 0x0E-structures_typedef/1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,6 +1,14 @@
-#include "dog.h"
 #include <stdlib.h>
-
+#include "dog.h"
+/**
+ * init_dog - initialize a variable of type struct dog
+ * @d: pointer to struct dog
+ * @name: pointer to name of dog
+ * @age: age of dog
+ * @owner: pointer to owner of dog
+ *
+ * Return: pointer to struct dog
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
      if (d == NULL)
