134ba645faafae76beef9fed94bbde550b27c331
Hassan220022 <hassansonson2002@gmail.com>
2023-11-07 09:46:55 +0200

    0x0E-structures_typedef/1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -17,9 +17,4 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
      d->name = name;
      d->age = age;
      d->owner = owner;
-
-     if (d->name == NULL && d->owner == NULL && d->age == 0)
-     {
-          return;
-     }
 }
