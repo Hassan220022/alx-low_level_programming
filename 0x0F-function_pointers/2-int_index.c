ab1e10cd52d974331eec044547b5325f46aefe4a
Hassan220022 <hassansonson2002@gmail.com>
2023-11-08 17:48:12 +0200

    bor3i

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,12 +1,12 @@
 #include "function_pointers.h"
 
 /**
- * int_index - indexes aray i think
- * @array: array to go through
- * @size: size of array
- * @cmp: function to do something with size
- * Return: int value
- */
+* int_index - indexes aray i think
+* @array: array to go through
+* @size: size of array
+* @cmp: function to do something with size
+* Return: int value
+*/
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
@@ -19,6 +19,5 @@ int int_index(int *array, int size, int (*cmp)(int))
 				return (i);
 		}
 	}
-	else
-		return (-1);
+	return (-1);
 }
