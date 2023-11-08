5dc0a31aa4362ce03af247098c324dfa0b301468
Hassan220022 <hassansonson2002@gmail.com>
2023-11-08 15:38:19 +0200

    bor3i

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+* print_name - uses pointer to a funtion to print name
+* @name: name to print
+* @f: pointer to function
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	if (!(name && f))
+		f(name);
+}
