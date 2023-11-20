5e2a1e6be8307650ca9bc6e66ced1de4f473e3de
Hassan220022 <hassansonson2002@gmail.com>
2023-11-20 15:07:31 +0200

    asm

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,20 @@
+section .data
+    hello db "Hello, Holberton", 0
+    format db "%s", 0
+
+section .text
+    extern printf
+
+global main
+main:
+    push rbp
+    mov rbp, rsp
+
+    push hello
+    push format
+    call printf
+    add rsp, 16
+
+    mov eax, 0
+    leave
+    ret
