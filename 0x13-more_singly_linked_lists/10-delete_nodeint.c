5094dac8993061a3f6208a2f0d77894cacb21b82
Hassan220022 <hassansonson2002@gmail.com>
2023-11-21 00:07:45 +0200

    0x13-more_singly_linked_lists/10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -6,8 +6,6 @@
 * @head: pointer to pointer to head of list
 * @index: index of the node that should be deleted
 * Return: 1 if it succeeded, -1 if it failed
-* Note: index starts at 0
-* Note: if it is not possible to delete the node at index index, do not
 */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
